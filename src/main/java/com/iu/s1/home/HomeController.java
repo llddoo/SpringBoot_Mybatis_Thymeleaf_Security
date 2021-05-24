da0b3077f45a7a670207a88c98ad114dbef2ad26
이석준 <llddoo@naver.com>
2021-05-24 09:45:17 +0900

    Initial commit

---
+++ src/main/java/com/iu/s1/home/HomeController.java
@@ -0,0 +1,13 @@
+package com.iu.s1.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
